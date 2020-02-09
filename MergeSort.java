c03dccc8578cce746724a80650efd3288b81ab36
gtoledorodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2020-02-09 15:48:57 -0600

    added code to sort

--- MergeSort.java
+++ MergeSort.java
@@ -1,6 +1,29 @@
 public class MergeSort{
   public int[] sort(int[] arr){//This will sort the left and right arrays
+    int[] sorted = []; //Return our final answer, after merging and sorting the left and right arrays
+    int arrL = arr.length; //gives the length of the user given arr
 
+    if (arrL == 1) { //If arrL is 1, there is only one in in the array so return itself
+      sorted = arr;
+    }
+    else{
+      int m = arrL/2; //Find the middle so we can separate array into left and right arrays
+
+      int[] leftArr = [m]; //After original array is divided this will be the left side of the arrays
+      int[] rightArr = [m]; //After original array is divided this will be the right side of the arrays
+
+      for(int i = 0; i<=m; i++){ // left arr will be from 0 to m
+        leftArr[i]=arr[i];
+      }
+      for(int i = m+1; i<=arrL; i++){ // right arr will be from m+1 to arrL
+        rightArr[i]=arr[i];
+      }
+
+      sort(leftArr);
+      sort(rightArr);
+    }
+
+    return sorted;
   }
   public int[] merge(){//This will merge the left and right arrays
 
